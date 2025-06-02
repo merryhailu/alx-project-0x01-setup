d4152147380a143d763efe128286b40b0907d409
Meron Hailu <hailumeron03@gmail.com>
2025-06-02 16:28:09 +0300

    feat: adding week 4

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { UserProps } from '@/interfaces';
+
+const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
+  return (
+    <div className="border p-4 rounded shadow-md w-full max-w-md">
+      <h2 className="text-xl font-semibold">{user.name} (@{user.username})</h2>
+      <p className="text-sm text-gray-600">{user.email}</p>
+      <p className="mt-2">📍 {user.address.city}, {user.address.street}</p>
+      <p>📞 {user.phone}</p>
+      <p>🌐 <a href={`http://${user.website}`} target="_blank" rel="noopener noreferrer" className="text-blue-500">{user.website}</a></p>
+      <p className="mt-2 font-medium">🏢 {user.company.name}</p>
+    </div>
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,14 +1,36 @@
-import Header from "@/components/layout/Header";
+import { GetStaticProps } from 'next';
+import React from 'react';
+import { UserProps } from '@/interfaces';
+import UserCard from '@/components/common/UserCard';
+import Header from '@/components/layout/Header';
 
-const UsersPage: React.FC = () => {
-    return (
-        <>
-            <Header />
-            <div className="p-8">
-                <h1 className="text-3xl mb-4">Users Page</h1>
-            </div>
-        </>
-    );
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const UsersPage: React.FC<UsersPageProps> = ({ posts }) => {
+  return ( 
+    <>
+    <Header />
+    <div className="p-8 flex flex-col items-center gap-4">
+      <h1 className="text-3xl mb-4 font-bold">Users Page</h1>
+      {posts.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
   };
-  
-  export default UsersPage;
+};
+
+export default UsersPage;
